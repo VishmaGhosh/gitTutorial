caf52c8e6f1d8ec541e60b505251de4204beae44
VishmaGhosh <vishmaghosh9046@gmail.com>
2022-03-22 14:48:41 +0530

    Create Node and modify Node

--- form.js
+++ form.js
@@ -1,16 +1,16 @@
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.all);
-document.all[6].textContent = "My List";
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.all);
+// document.all[6].textContent = "My List";
 
-console.log(document.getElementById('headerTitle'));
-var headerTitle = document.getElementById('headerTitle');
-headerTitle.textContent = "Hello";
-headerTitle.style.borderBottom = '3px solid #000'
+// console.log(document.getElementById('headerTitle'));
+// var headerTitle = document.getElementById('headerTitle');
+// headerTitle.textContent = "Hello";
+// headerTitle.style.borderBottom = '3px solid #000'
 
-document.getElementById('addItem').style.color = "red";
+// document.getElementById('addItem').style.color = "red";
 
 // get Element by className
 
@@ -41,12 +41,81 @@ document.getElementById('addItem').style.color = "red";
 
 // Get element by queryselector
 
-var items = document.querySelectorAll('.list-group-item');
-items[1].style.color = "green"
+// var items = document.querySelectorAll('.list-group-item');
+// items[1].style.color = "green"
 
-var odds = document.querySelectorAll('.list-group-item:nth-child(odd)')
-console.log(odds);
-for(var i = 0; i<odds.length;i++){
-    odds[i].style.backgroundColor = "green";
-}
+// var odds = document.querySelectorAll('.list-group-item:nth-child(odd)')
+// console.log(odds);
+// for(var i = 0; i<odds.length;i++){
+//     odds[i].style.backgroundColor = "green";
+// }
+
+var items = document.querySelector('#items');
+// parentElement
+// console.log(items.parentNode);
+// items.parentNode.style.backgroundColor = "#f5f5f5";
+// console.log(items.parentNode.parentNode);
+
+// parent Element
+
+// console.log(items.parentElement);
+// items.parentElement.style.backgroundColor = "#f5f5f5";
+// console.log(items.parentElement.parentElement);
+
+// child node
+// console.log(items.childNodes);
+
+console.log(items.children);
+
+// First Child
+console.log(items.firstChild);
+
+// First Element child
+console.log(items.firstElementChild);
+
+// Last child 
+console.log(items.lastChild);
+
+// Last Element Child
+console.log(items.lastElementChild);
+
+// Next Sibling
+console.log(items.nextSibling);
+
+// Next Element sibling
+console.log(items.nextElementSibling);
+
+// Previous Sibling
+console.log(items.previousSibling);
+
+// previous Element sibling
+console.log(items.previousElementSibling);
+items.previousElementSibling.style.color = "green";
+
+// Create Element
+
+var newDiv = document.createElement('div');
+// add class name
+newDiv.className = 'hello';
+//add attribute
+newDiv.setAttribute('title', 'hello div');
+
+// create text node
+var newDivText = document.createTextNode('Hello world!')
+
+// add text to div
+newDiv.appendChild(newDivText);
+console.log(newDiv);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1')
+container.insertBefore(newDiv,h1)
+
+// insert before item 1
+
+var ul = document.querySelector('ul');
+console.log(ul);
+var item1 = document.querySelector('ul .ulDiv');
+ul.insertBefore(newDiv, item1);
+
 
