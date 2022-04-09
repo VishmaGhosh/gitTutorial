74f3d77fc2c35c2133b558304cb3c45231c1d428
VishmaGhosh <vishmaghosh9046@gmail.com>
2022-04-09 09:43:42 +0530

    Add Expenses using crudcrud api

--- addExpense.js
+++ addExpense.js
@@ -5,19 +5,26 @@ const catagory = document.querySelector('#catagory');
 var list = document.getElementById('my-list');
 
 list.addEventListener('click', removeItem);
+window.addEventListener('DOMContentLoaded', showExpenses());
 
 const key = Object.keys(localStorage);
 let i = key.length;
-while (i--) {
-
-    let expense = JSON.parse(localStorage.getItem(key[i]));
-    // console.log(key[i]);
-    const li = `<li id=${key[i]} class="list-group-item"> <span>${expense.expense}</span>--<span>${expense.desc}</span>--<span>${expense.catagory}</span>
+function showExpenses() {
+    list.innerHTML = "";
+    axios.get('https://crudcrud.com/api/9e8214116d0d4a6fa032e297172cef48/expenses')
+        .then(res => {
+            res.data.map((expense, key) => {
+                const li = `<li id=${expense._id} class="list-group-item"> <span>${expense.expense}</span>--<span>${expense.desc}</span>--<span>${expense.catagory}</span>
     <button class="btn btn-danger btn-sm m-1 delete">delete</button>
     <button class="btn btn-warning btn-sm m-1 edit">edit</button></li>`
-    list.innerHTML = list.innerHTML + li;
-    // list.appendChild(li);
+                list.innerHTML = list.innerHTML + li;
+            })
+        })
+        .catch(err => console.log(err));
+    // console.log(key[i]);
 }
+
+
 
 
 myform.addEventListener('submit', onSubmit)
@@ -34,12 +41,9 @@ function onSubmit(e) {
             desc: desc.value,
             catagory: catagory.value
         }
-        localStorage.setItem(`item${localStorage.length}`, JSON.stringify(newExpense));
-        const id = `item${localStorage.length}`
-        const li2 = `<li id=${id} class="list-group-item"> <span>${newExpense.expense}</span>--<span>${newExpense.desc}</span>--<span>${newExpense.catagory}</span>
-                    <button class="btn btn-danger btn-sm m-1 delete">delete</button>
-                    <button class="btn btn-warning btn-sm m-1 edit">edit</button></li>`
-        list.innerHTML = list.innerHTML + li2;
+        axios.post('https://crudcrud.com/api/9e8214116d0d4a6fa032e297172cef48/expenses', newExpense)
+            .then(res => showExpenses())
+            .catch(err => console.log(err));
 
         // clear field
         expense.value = "";
@@ -53,20 +57,22 @@ function removeItem(e) {
         var li = e.target.parentElement;
         var x = li.id;
         console.log(x);
-        localStorage.removeItem(x);
-        list.removeChild(li);
+        axios.delete(`https://crudcrud.com/api/9e8214116d0d4a6fa032e297172cef48/expenses/${x}`)
+            .then(res => showExpenses())
+            .catch(err => console.log(err))
     }
 
     if (e.target.classList.contains('edit')) {
         var li = e.target.parentElement;
-        console.log(li);
+        let x = li.id;
         var exp = li.childNodes[1].textContent;
         var des = li.childNodes[3].textContent;
         var cat = li.childNodes[5].textContent;
-        console.log(exp);
+        axios.delete(`https://crudcrud.com/api/9e8214116d0d4a6fa032e297172cef48/expenses/${x}`)
+            .then((res) => showExpenses())
+            .catch((err) => console.log(err));
         expense.value = exp;
         desc.value = des;
         catagory.value = cat;
-        list.removeChild(li);
     }
 }
