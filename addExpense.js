bf5d04a20ba98db0cb4602dcd3cf4414075532e8
VishmaGhosh <vishmaghosh9046@gmail.com>
2022-04-13 11:50:24 +0530

    axios get method with try and catch

--- addExpense.js
+++ addExpense.js
@@ -9,19 +9,34 @@ window.addEventListener('DOMContentLoaded', showExpenses());
 
 const key = Object.keys(localStorage);
 let i = key.length;
-function showExpenses() {
+
+async function showExpenses() {
     list.innerHTML = "";
-    axios.get('https://crudcrud.com/api/9e8214116d0d4a6fa032e297172cef48/expenses')
-        .then(res => {
-            res.data.map((expense, key) => {
-                const li = `<li id=${expense._id} class="list-group-item"> <span>${expense.expense}</span>--<span>${expense.desc}</span>--<span>${expense.catagory}</span>
-    <button class="btn btn-danger btn-sm m-1 delete">delete</button>
-    <button class="btn btn-warning btn-sm m-1 edit">edit</button></li>`
-                list.innerHTML = list.innerHTML + li;
-            })
-        })
-        .catch(err => console.log(err));
+    // axios.get('https://crudcrud.com/api/0443a2abfd2942f6b0bc41c8e3c331a4/expenses')
+    //     .then(res => {
+    //         res.data.map((expense, key) => {
+    //             const li = `<li id=${expense._id} class="list-group-item"> <span>${expense.expense}</span>--<span>${expense.desc}</span>--<span>${expense.catagory}</span>
+    // <button class="btn btn-danger btn-sm m-1 delete">delete</button>
+    // <button class="btn btn-warning btn-sm m-1 edit">edit</button></li>`
+    //             list.innerHTML = list.innerHTML + li;
+    //         })
+    //     })
+    //     .catch(err => console.log(err));
     // console.log(key[i]);
+
+
+    try {
+        let res =  await axios.get('https://crudcrud.com/api/26958c84cae049bab516fb1733600b56/expenses')
+        console.log(res.data);
+        res.data.map((expense, key) => {
+            const li = `<li id=${expense._id} class="list-group-item"> <span>${expense.expense}</span>--<span>${expense.desc}</span>--<span>${expense.catagory}</span>
+<button class="btn btn-danger btn-sm m-1 delete">delete</button>
+<button class="btn btn-warning btn-sm m-1 edit">edit</button></li>`
+            list.innerHTML = list.innerHTML + li;
+        })
+    } catch (error) {
+        console.log("Error");
+    }
 }
 
 
@@ -29,7 +44,7 @@ function showExpenses() {
 
 myform.addEventListener('submit', onSubmit)
 
-function onSubmit(e) {
+async function onSubmit(e) {
     e.preventDefault();
     if (expense.value === '' || desc.value === '' || catagory === '') {
         alert("Enter all the value");
@@ -41,9 +56,16 @@ function onSubmit(e) {
             desc: desc.value,
             catagory: catagory.value
         }
-        axios.post('https://crudcrud.com/api/9e8214116d0d4a6fa032e297172cef48/expenses', newExpense)
-            .then(res => showExpenses())
-            .catch(err => console.log(err));
+        try {
+            let res = await axios.post('https://crudcrud.com/api/26958c84cae049bab516fb1733600b56/expenses', newExpense)
+            console.log(res);
+            if(res.status === 201){
+                console.log("New user is created");
+                showExpenses();
+            }
+        } catch (error) {
+            console.log(error);
+        }
 
         // clear field
         expense.value = "";
@@ -52,14 +74,19 @@ function onSubmit(e) {
     }
 }
 
-function removeItem(e) {
+async function removeItem(e) {
     if (e.target.classList.contains('delete')) {
         var li = e.target.parentElement;
         var x = li.id;
         console.log(x);
-        axios.delete(`https://crudcrud.com/api/9e8214116d0d4a6fa032e297172cef48/expenses/${x}`)
-            .then(res => showExpenses())
-            .catch(err => console.log(err))
+        try {
+            let res = await axios.delete(`https://crudcrud.com/api/26958c84cae049bab516fb1733600b56/expenses/${x}`)
+                if(res.status === 200)
+                    showExpenses();
+        } catch (error) {
+            console.log(error);
+        }
+        
     }
 
     if (e.target.classList.contains('edit')) {
@@ -68,7 +95,7 @@ function removeItem(e) {
         var exp = li.childNodes[1].textContent;
         var des = li.childNodes[3].textContent;
         var cat = li.childNodes[5].textContent;
-        axios.delete(`https://crudcrud.com/api/9e8214116d0d4a6fa032e297172cef48/expenses/${x}`)
+        axios.delete(`https://crudcrud.com/api/26958c84cae049bab516fb1733600b56/expenses/${x}`)
             .then((res) => showExpenses())
             .catch((err) => console.log(err));
         expense.value = exp;
